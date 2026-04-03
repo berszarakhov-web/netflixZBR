2a6b183b5e6e38df0066dbb3513b9cf304f4975e
berszarakhov-web <berszarakhov@gmail.com>
2026-04-03 14:50:01 +0500

    fix4

--- project/backend/createAdmin.js
+++ project/backend/createAdmin.js
@@ -2,7 +2,7 @@
 // Запуск: node createAdmin.js
 require('dotenv').config()
 const mongoose = require('mongoose')
-const bcrypt   = require('bcrypt')
+const bcrypt   = require('bcryptjs')
 const User     = require('./models/User')
 
 const createAdmin = async () => {
